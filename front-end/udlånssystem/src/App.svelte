a347395a808421554c859d3e2685885ed937a86c
Kenni Kollemorten <kenn7575@gmail.com>
2023-05-12 08:21:40 +0200

    addedd front-end project for "udlånssystem"

---
+++ front-end/udlånssystem/src/App.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { Router, Route } from "svelte-routing";
+  import Nav from "./components/Nav.svelte";
+  import Home from "./pages/Home.svelte";
+  import Users from "./pages/Users.svelte";
+  import Error404 from "./pages/Error404.svelte";
+</script>
+
+<div class="app">
+  <Router>
+    <!-- navigation -->
+    <header>
+      <Nav />
+    </header>
+    <!-- routes -->
+    <main>
+      <Route component={Error404} />
+      <Route path="/" component={Home} />
+      <Route path="home" component={Home} />
+      <Route path="users" component={Users} />
+    </main>
+  </Router>
+</div>
+
+<style>
+  .app {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    height: 100vh;
+    background: var(--bg-color-1);
+  }
+  header {
+    background: var(--nav-color);
+    width: 70px;
+    transition: width 0.3s ease-in-out;
+    border-radius: 0 15px 15px 0;
+  }
+  main {
+    background-color: var(--bg-color-1);
+  }
+  header:hover {
+    width: 250px;
+  }
+</style>
---
+++ front-end/udlånssystem/tests/dataFlow.test.ts
@@ -0,0 +1,7 @@
+import {it, describe, expect} from 'vitest';
+
+describe('dataFlow', () => {
+    it('should work', () => {
+        expect(1 + 1).toBe(2);
+    });
+});
