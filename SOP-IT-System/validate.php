8543a6dc974fd67e3574d306727189b211326b75
Nima - Skole <nima0100@edu.sde.dk>
2023-06-06 14:05:37 +0200

    Delete login_sessions after expiration date

--- SOP-IT-System/validate.php
+++ SOP-IT-System/validate.php
@@ -4,6 +4,7 @@ include "headers.php";
 $header = apache_request_headers()['Authorization'] ?? $_GET['auth'] ?? "";
 
 $conn = new mysqli("127.0.0.1", $env['ADMIN_USERNAME'], $env['ADMIN_PASSWORD'], "sop", '3306');
+$conn->query("DELETE FROM `login_sessions` WHERE expiration_date < NOW()");
 
 $token = $_POST['token'] ?? "";
 
