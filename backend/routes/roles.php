4429823eeaff5a1e62fc2f8b35ba1c2d85f35d5b
Nima - Skole <nima0100@edu.sde.dk>
2023-06-19 11:00:51 +0200

    Update_userV2

--- backend/routes/roles.php
+++ backend/routes/roles.php
@@ -5,7 +5,7 @@ $roles = $conn->query("SELECT * FROM `roles`")->fetch_all(MYSQLI_ASSOC);
 
 for ($i=0; $i < count($roles); $i++) { 
   $auth_lvl_id = $roles[$i]['authorization_level_id'];
-  $roles[$i]['authorization_level_id'] = $conn->query("SELECT * FROM `authorization_level` WHERE `UUID` = $auth_lvl_id")->fetch_object();
+  $roles[$i]['authorization_level_id'] = $conn->query("SELECT * FROM `authorization_levels` WHERE `UUID` = $auth_lvl_id")->fetch_object();
 }
 
 echo json_encode($roles, JSON_PRETTY_PRINT);
---
+++ backend/routes/user_data.php
@@ -0,0 +1,19 @@
+<?php
+include "admin_db_conn.php";
+
+$UUID_query = "";
+$table = $_GET['table'] ?? "users";
+
+if (isset($_GET['UUID'])) $UUID_query = "WHERE `UUID` = $_GET[UUID]";
+
+$column = $conn->query("SELECT * FROM `$table` $UUID_query")->fetch_object();
+
+foreach ($column as $key => $value) {
+  if (substr($key, -3) == "_id" && isset($value)) {
+    $table_name = substr($key, 0, -3);
+    if (substr($table_name, -1) == "s") { $table_name = $table_name . "es"; } else { $table_name = $table_name . "s"; }
+    $column->$key = $conn->query("SELECT * FROM `$table_name` WHERE `UUID` = $value")->fetch_object();
+  }
+}
+
+echo json_encode($column, JSON_PRETTY_PRINT);
