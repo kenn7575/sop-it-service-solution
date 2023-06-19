0891057781f89bedad0629da3a478ca236df1555
Kenni Kollemorten <kenn7575@gmail.com>
2023-06-19 15:27:14 +0200

    users maybe fix? v1

---
+++ frontend/udlånssystem/src/pages/users/new_old.svelte
@@ -0,0 +1,395 @@
+<script lang="ts">
+  import DropZone from "../../components/drop-zone.svelte";
+  import { onMount } from "svelte";
+  import { getData } from "../../data/data";
+  import validate from "../../services/validateNewUser.js";
+  import type { UserModel } from "../../types/userModel";
+
+  let exportUser: UserModel;
+
+  let roles = [];
+  let educations = [];
+
+  onMount(async () => {
+    roles = await getData("roles");
+    educations = await getData("educations");
+    exportUser.education_id = educations[0].UUID;
+    exportUser.role = roles[3].UUID;
+  });
+
+  let errorMessages = "";
+
+  import { currentUser, loginViaCredentials } from "../../services/login";
+  import axios from "axios";
+  // $: console.log("user", $currentUser);
+
+  async function login(e) {
+    e.preventDefault();
+  }
+  function resetError() {
+    errorMessages = "";
+  }
+  function validateEmail(email) {}
+
+  function clearPicture() {
+    new_user.profilePicture = "";
+  }
+  function handleFileDrop(event) {
+    new_user.profilePicture = event.detail;
+  }
+
+  function handleSubmit(e) {
+    if (validate(new_user)) {
+      return (errorMessages = validate(new_user));
+    }
+
+    console.log(new_user);
+    axios
+      .post("/create_user.php", { new_user: JSON.stringify(new_user) })
+      .then((res) => {
+        if (res) console.log("Bruger oprettet");
+      });
+  }
+</script>
+
+<div class="content">
+  <div class="image-upload">
+    <!-- svelte-ignore a11y-img-redundant-alt -->
+    <img
+      src={new_user.profilePicture
+        ? new_user.profilePicture
+        : "https://img.freepik.com/free-icon/user_318-563642.jpg"}
+      alt="Profile picture"
+    />
+    <div class="buttons">
+      <button disabled={new_user.profilePicture === ""} on:click={clearPicture}
+        >Slet</button
+      >
+      <button on:click={handleSubmit}>Gem</button>
+    </div>
+    <DropZone on:message={handleFileDrop} />
+  </div>
+  <div class="form">
+    <form id="user-form" on:submit={login}>
+      <div class="question">
+        <label for="a1" class:error={errorMessages}
+          >Fornavn <span>*</span></label
+        >
+        <input
+          id="firstName"
+          class:error={errorMessages}
+          on:focus={resetError}
+          bind:value={new_user.firstName}
+          autocomplete="off"
+          class="text"
+          type="text"
+          required
+        />
+      </div>
+      <div class="question" class:error={errorMessages}>
+        <label for="a2" class:error={errorMessages}
+          >Efternavn <span>*</span></label
+        >
+        <input
+          id="a2"
+          autocomplete="off"
+          class:error={errorMessages}
+          on:focus={resetError}
+          bind:value={new_user.lastName}
+          class="text"
+          type="text"
+          required
+        />
+      </div>
+      <div class="question" class:error={errorMessages}>
+        <label for="a3" class:error={errorMessages}
+          >Adgangskode <span>*</span></label
+        >
+        <input
+          id="a3"
+          autocomplete="off"
+          class:error={errorMessages}
+          on:focus={resetError}
+          bind:value={new_user.password}
+          class="text"
+          type="password"
+          required
+        />
+      </div>
+      <div class="question" class:error={errorMessages}>
+        <label for="a4" class:error={errorMessages}
+          >Uni-login <span>*</span></label
+        >
+        <input
+          id="a4"
+          autocomplete="off"
+          class:error={errorMessages}
+          on:focus={resetError}
+          bind:value={new_user.unilogin}
+          class="text"
+          type="text"
+          required
+        />
+      </div>
+      <div class="question" class:error={errorMessages}>
+        <label for="a5" class:error={errorMessages}>E-mail <span>*</span></label
+        >
+        <input
+          id="a5"
+          autocomplete="off"
+          class:error={errorMessages}
+          on:focus={resetError}
+          bind:value={new_user.mail}
+          class="text"
+          type="text"
+          required
+        />
+      </div>
+      <div class="question" class:error={errorMessages}>
+        <label for="a6" class:error={errorMessages}
+          >Vejnavn <span>*</span></label
+        >
+        <input
+          id="a6"
+          autocomplete="off"
+          class:error={errorMessages}
+          on:focus={resetError}
+          bind:value={new_user.street1}
+          class="text"
+          type="text"
+          required
+        />
+      </div>
+      <div class="question" class:error={errorMessages}>
+        <label for="a7" class:error={errorMessages}>Etage </label>
+        <input
+          id="a7"
+          autocomplete="off"
+          class:error={errorMessages}
+          on:focus={resetError}
+          bind:value={new_user.street2}
+          class="text"
+          type="text"
+        />
+      </div>
+      <div class="question" class:error={errorMessages}>
+        <label for="a8" class:error={errorMessages}>City <span>*</span></label>
+        <input
+          id="a8"
+          autocomplete="off"
+          class:error={errorMessages}
+          on:focus={resetError}
+          bind:value={new_user.city}
+          class="text"
+          type="text"
+          required
+        />
+      </div>
+      <div class="question" class:error={errorMessages}>
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <label class:error={errorMessages}>Postnummer <span>*</span></label>
+        <input
+          autocomplete="off"
+          class:error={errorMessages}
+          on:focus={resetError}
+          bind:value={new_user.zip}
+          class="text"
+          id="postnummer"
+          pattern="[0-9]{4}"
+          type="number"
+          required
+        />
+      </div>
+      <div class="question">
+        <label for="role" class:error={errorMessages}
+          >Bruger rolle <span>*</span></label
+        >
+
+        <select id="role" required form="user-form" bind:value={new_user.role}>
+          <option selected disabled>Vælg fra liste</option>
+          {#each roles as role}
+            <option id="role" value={role.UUID}>{role.name}</option>
+          {/each}
+        </select>
+      </div>
+
+      <div class="question">
+        <label for="education" class:error={errorMessages}
+          >Uddannelse<span>*</span></label
+        >
+
+        <select
+          id="education"
+          required
+          form="user-form"
+          bind:value={new_user.education}
+        >
+          <option selected disabled>Vælg fra liste</option>
+          {#each educations as education}
+            <option id="education" value={education.UUID}
+              >{education.name}</option
+            >
+          {/each}
+        </select>
+      </div>
+    </form>
+  </div>
+</div>
+
+<style>
+  .buttons {
+    display: flex;
+    width: 100%;
+    gap: 0.5rem;
+    justify-content: space-between;
+  }
+  select {
+    width: 100%;
+    background: transparent;
+    color: var(--text1);
+    height: 40px;
+    border: var(--text1) 1px solid;
+    border-radius: 10px;
+    background: var(--bg1);
+    font-size: 1rem;
+    padding: 10px 15px;
+  }
+
+  .content {
+    height: 100%;
+    box-sizing: border-box;
+    padding: 2rem;
+    display: flex;
+    gap: 1rem;
+  }
+  img {
+    max-width: 90%;
+    object-fit: cover;
+    aspect-ratio: 1 / 1;
+    border-radius: 50%;
+  }
+  .image-upload {
+    width: max(35%, 400px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+    background-color: var(--bg2);
+    border-radius: 10px;
+    padding: 1rem;
+  }
+  .form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    overflow-y: auto;
+  }
+  button {
+    width: 100%;
+    height: 2rem;
+    color: var(--text1);
+    background: transparent;
+    border: 1px solid var(--text1);
+    border-radius: 10px;
+  }
+  button:disabled {
+    opacity: 0.5;
+  }
+
+  span {
+    position: absolute;
+    transform: translate(5px, -5px);
+    color: var(--s);
+    font-size: 2rem;
+    font-weight: 400;
+  }
+
+  form {
+    position: relative;
+    display: inline-block;
+    max-width: 700px;
+
+    box-sizing: border-box;
+
+    background: transparent;
+    border-radius: 40px;
+
+    left: 50%;
+    -moz-transform: translate(-50%, 0);
+    -ms-transform: translate(-50%, 0);
+    -webkit-transform: translate(-50%, 0);
+    transform: translate(-50%, 0);
+  }
+
+  form button {
+    margin-top: 35px;
+    background-color: transparent;
+    border: 2px solid var(--text1);
+    line-height: 0;
+    font-size: 17px;
+    display: inline-block;
+    box-sizing: border-box;
+    padding: 20px 15px;
+    border-radius: 60px;
+    color: var(--text1);
+    font-weight: 400;
+    letter-spacing: 0.01em;
+    position: relative;
+    z-index: 1;
+  }
+  form button:hover,
+  form button:focus {
+    color: #fff;
+    background-color: var(--p);
+  }
+
+  form .question label {
+    transform-origin: left center;
+    color: var(--text1);
+    font-weight: 300;
+    letter-spacing: 0.01em;
+    font-size: 1rem;
+    box-sizing: border-box;
+    padding: 10px 15px;
+    display: block;
+    z-index: 2;
+    pointer-events: none;
+  }
+  form .question input.text {
+    appearance: none;
+    background: none;
+    border: 1px solid var(--text1);
+    line-height: 0;
+    font-size: 17px;
+    width: 100%;
+    display: block;
+    box-sizing: border-box;
+    padding: 10px 15px;
+    border-radius: 10px;
+    color: var(--text1);
+    font-weight: 500;
+    letter-spacing: 0.01em;
+    position: relative;
+    z-index: 1;
+  }
+
+  form .question input.text:focus {
+    outline: none;
+    border: 2px solid var(--i);
+    background: var(--bg2);
+    color: var(--text1);
+  }
+
+  form .question .error {
+    border-color: var(--s) !important;
+    color: var(--s) !important;
+  }
+  .question {
+    margin-bottom: 1rem;
+  }
+  .errorMessage {
+    color: var(--s) !important;
+    font-weight: 400 !important;
+  }
+</style>
