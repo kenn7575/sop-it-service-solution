4f94144c47b3b5d54eef651fa64456efa5d88c79
Kenni Kollemorten <kenn7575@gmail.com>
2023-06-23 14:23:25 +0200

    extreme refactoring

---
+++ frontend/udlånssystem/src/components/textQuestion.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let editMode = false;
+  export let label;
+  export let type = "text";
+  export let binding;
+  $: console.log(binding);
+</script>
+
+<div class="question">
+  <label
+    >{label} <span class="required-tag" class:hidden={!editMode}>*</span></label
+  >
+  {#if type === "text"}
+    <input
+      autocomplete="off"
+      disabled={!editMode}
+      bind:value={binding}
+      class="text"
+      type="text"
+      required
+    />
+  {:else if type === "number"}
+    <input
+      autocomplete="off"
+      disabled={!editMode}
+      bind:value={binding}
+      class="text"
+      type="number"
+      required
+    />
+  {:else if type === "password"}
+    <input
+      autocomplete="off"
+      disabled={!editMode}
+      bind:value={binding}
+      class="text"
+      type="password"
+      required
+    />
+  {/if}
+</div>
--- frontend/udlånssystem/src/pages/users/edit.svelte
+++ frontend/udlånssystem/src/pages/users/edit.svelte
@@ -6,15 +6,17 @@
   import { currentUser } from "../../services/login";
   import { getData } from "../../data/data";
   import validateInputs from "../../services/validateInputs.js";
-  import doseObjectsMach from "../../services/doseObjectsMach.js";
+  import doesObjectsMatch from "../../services/doesObjectsMatch.js";
   import deleteItem from "../../services/deleteItemFromDB";
+  import togggleEditMode from "../../services/toggleEditMode";
+  import TextQuestion from "../../components/textQuestion.svelte";
+  import NumberQuestion from "../../components/numberQuestion.svelte";
+  import SelectQuestion from "../../components/selectQuestion.svelte";
+  export let id; //this is the id of the user to be edited
 
-  //this is the id of the user to be edited
-  export let id;
-  $: user = $currentUser;
+  $: user = $currentUser; // the current user - used to check if the user has the right authorization level to edit this user
 
-  //if the page is in edit mode or read only
-  let editMode = false;
+  let editMode = false; //if the page is in edit mode or read only
 
   //the picture to be uploaded
   let picture;
@@ -22,18 +24,7 @@
   //imported Data
   let roles = [];
   let educations = [];
-
   let importData: UserModel;
-
-  let name;
-  let username;
-  let mail;
-  let address_line_1;
-  let address_line_2;
-  let city;
-  let postal_code;
-  let role_id;
-  let education_id;
 
   let new_name;
   let new_username;
@@ -50,7 +41,6 @@
     try {
       roles = await getData("roles_view").then((res) => {
         res.map((role) => (role.UUID = role.UUID.toString()));
-
         return res;
       });
 
@@ -70,7 +60,6 @@
       .then((res) => {
         return res.data;
       });
-
     if (importData.address_id === null)
       importData.address_id = {
         UUID: null,
@@ -79,44 +68,18 @@
         city: null,
         postal_code: null,
       };
-
-    asignValuesToUser(importData);
-    asignValueToNewUser();
-  }
-
-  function asignValuesToUser(importUser) {
-    name = importUser.name;
-    username = importUser.username;
-    mail = importUser.mail;
-    address_line_1 = importUser.address_id.address_line_1;
-    address_line_2 = importUser.address_id.address_line_2;
-    city = importUser.address_id.city;
-    postal_code = importUser.address_id.postal_code;
-    role_id = importUser.role_id.UUID;
-    education_id = importUser.education_id.UUID;
-  }
-
-  function asignValueToNewUser() {
-    new_name = name;
-    new_username = username;
-    new_mail = mail;
-    new_address_line_1 = address_line_1;
-    new_address_line_2 = address_line_2;
-    new_city = city;
-    new_postal_code = postal_code;
-    new_role_id = role_id;
-    new_education_id = education_id;
+    resetPage();
   }
   function resetPage() {
-    new_name = name;
-    new_username = username;
-    new_mail = mail;
-    new_address_line_1 = address_line_1;
-    new_address_line_2 = address_line_2;
-    new_city = city;
-    new_postal_code = postal_code;
-    new_role_id = role_id;
-    new_education_id = education_id;
+    new_name = importData.name;
+    new_username = importData.username;
+    new_mail = importData.mail;
+    new_address_line_1 = importData.address_id.address_line_1;
+    new_address_line_2 = importData.address_id.address_line_2;
+    new_city = importData.address_id.city;
+    new_postal_code = importData.address_id.postal_code;
+    new_role_id = importData.role_id.UUID;
+    new_education_id = importData.education_id.UUID;
     picture = "";
   }
   function handleUpdate() {
@@ -124,38 +87,17 @@
       alert("Udfyld alle felter");
       return;
     }
+    //prepere data for comparison
+    var oldObject: any = { ...importData };
+    delete oldObject.date_created;
+    delete oldObject.date_updated;
+    delete oldObject.address_id.date_created;
+    delete oldObject.address_id.date_updated;
+    delete oldObject.is_ad_user;
+    oldObject.education_id = oldObject.education_id.UUID;
+    oldObject.role_id = oldObject.role_id.UUID;
 
-    if (
-      doseObjectsMach(
-        {
-          name,
-          username,
-          mail,
-          address_line_1,
-          address_line_2,
-          city,
-          postal_code,
-          role_id,
-          education_id,
-        },
-        {
-          name: new_name,
-          username: new_username,
-          mail: new_mail,
-          address_line_1: new_address_line_1,
-          address_line_2: new_address_line_2,
-          city: new_city,
-          postal_code: new_postal_code,
-          role_id: new_role_id,
-          education_id: new_education_id,
-        }
-      )
-    ) {
-      alert("Ingen ændringer");
-      return;
-    }
-
-    let DataToBeUpdated: UserModel = {
+    const DataToBeUpdated = {
       UUID: importData.UUID,
       name: new_name,
       username: new_username,
@@ -172,7 +114,11 @@
       role_id: new_role_id,
       education_id: new_education_id,
     };
-    console.log(DataToBeUpdated);
+    console.log(importData);
+    if (doesObjectsMatch(oldObject, DataToBeUpdated)) {
+      alert("Ingen ændringer");
+      return;
+    }
     axios
       .post("upsert_user.php", DataToBeUpdated)
       .then((res) => {
@@ -188,21 +134,9 @@
         alert("Fejl! " + err);
       });
   }
-
-  function toggleEditMode() {
-    //check if user has the right authorization level to edit this user
-    if (
-      user.role_id.authorization_level_id <=
-      importData.role_id.authorization_level_id
-    ) {
-      alert(
-        `Du skal have højre rettighedder for at redigere denne bruger. Du har level ${user.role_id.authorization_level_id} rettighedder og brugeren har level ${importData.role_id.authorization_level_id}.`
-      );
-      return;
-    }
-    editMode = !editMode;
+  function handleEditMode() {
+    editMode = togggleEditMode(user, importData, editMode);
   }
-
   function clearPicture() {
     picture = "";
   }
@@ -228,6 +162,7 @@
 {#if importData}
   <div class="content">
     <div class="control-panel">
+      <!-- svelte-ignore a11y-img-redundant-alt -->
       <img
         src={picture
           ? picture
@@ -239,17 +174,16 @@
           ><i class="fa-solid fa-trash" /></button
         >
       {/if}
-
       <div class="buttons">
         <button
-          on:click={toggleEditMode}
+          on:click={handleEditMode}
           disabled={!editMode}
           on:click={resetPage}>Annuller</button
         >
         {#if editMode}
           <button on:click={handleUpdate}>Gem</button>
         {:else}
-          <button on:click={toggleEditMode}>Rediger</button>
+          <button on:click={handleEditMode}>Rediger</button>
         {/if}
       </div>
       {#if editMode}
@@ -257,114 +191,38 @@
         <DropZone on:message={handleFileDrop} />
       {/if}
     </div>
-
     <div class="form">
       <form on:submit={handleSubmit} id="user-form">
-        <div class="question">
-          <label for="a2"
-            >Navn <span class="required-tag" class:hidden={!editMode}>*</span
-            ></label
-          >
-          <input
-            id="a2"
-            disabled={!editMode}
-            autocomplete="off"
-            bind:value={new_name}
-            class="text"
-            type="text"
-            required
-          />
-        </div>
-
-        <div class="question">
-          <label for="a4"
-            >Uni-login <span class="required-tag" class:hidden={!editMode}
-              >*</span
-            ></label
-          >
-          <input
-            id="a4"
-            disabled={!editMode}
-            autocomplete="off"
-            bind:value={new_username}
-            class="text"
-            type="text"
-            required
-          />
-        </div>
-        <div class="question">
-          <label for="a5"
-            >E-mail <span class="required-tag" class:hidden={!editMode}>*</span
-            ></label
-          >
-          <input
-            id="a5"
-            disabled={!editMode}
-            autocomplete="off"
-            bind:value={new_mail}
-            class="text"
-            type="text"
-            required
-          />
-        </div>
-
-        <div class="question">
-          <label for="a6"
-            >Vejnavn <span class="required-tag" class:hidden={!editMode}>*</span
-            ></label
-          >
-          <input
-            id="a6"
-            disabled={!editMode}
-            autocomplete="off"
-            bind:value={new_address_line_1}
-            class="text"
-            type="text"
-            required
-          />
-        </div>
-        <div class="question">
-          <label for="a7">Etage mm.</label>
-          <input
-            id="a7"
-            disabled={!editMode}
-            autocomplete="off"
-            bind:value={new_address_line_2}
-            class="text"
-            type="text"
-          />
-        </div>
-        <div class="question">
-          <label for="a8"
-            >By <span class="required-tag" class:hidden={!editMode}>*</span
-            ></label
-          >
-          <input
-            id="a8"
-            disabled={!editMode}
-            autocomplete="off"
-            bind:value={new_city}
-            class="text"
-            type="text"
-            required
-          />
-        </div>
-        <div class="question">
-          <label for="a9"
-            >Postnummer <span class="required-tag" class:hidden={!editMode}
-              >*</span
-            ></label
-          >
-          <input
-            autocomplete="off"
-            disabled={!editMode}
-            bind:value={new_postal_code}
-            class="text"
-            id="a9"
-            type="number"
-            required
-          />
-        </div>
+        <TextQuestion bind:binding={new_name} label="Navn" {editMode} />
+        <TextQuestion
+          bind:binding={new_username}
+          label="Uni-login"
+          {editMode}
+        />
+        <TextQuestion bind:binding={new_mail} label="E-mail" {editMode} />
+        <TextQuestion
+          bind:binding={new_address_line_1}
+          label="Vejnavn"
+          {editMode}
+        />
+        <TextQuestion
+          bind:binding={new_address_line_2}
+          label="Etage mm."
+          {editMode}
+        />
+        <TextQuestion bind:binding={new_city} label="By" {editMode} />
+        <TextQuestion
+          bind:binding={new_postal_code}
+          label="Postnummer"
+          {editMode}
+        />
+        <SelectQuestion
+          bind:binding={new_role_id}
+          options={roles}
+          match={importData.role_id}
+          label="Bruger rolle"
+          {editMode}
+        />
         <div class="question">
           <label for="a10"
             >Bruger rolle <span class="required-tag" class:hidden={!editMode}
@@ -381,22 +239,21 @@
             >
               {#each roles as role}
                 {#if role.level < user.role_id.authorization_level_id}
-                  <option selected={role.UUID === role_id} value={role.UUID}
-                    >{role.name}</option
+                  <option
+                    selected={role.UUID === importData.role_id}
+                    value={role.UUID}>{role.name}</option
                   >
                 {/if}
               {/each}
             </select>
           {/if}
         </div>
-
         <div class="question">
           <label for="a11"
             >Uddannelse<span class="required-tag" class:hidden={!editMode}
               >*</span
             ></label
           >
-
           <select
             disabled={!editMode}
             id="a11"
@@ -406,7 +263,7 @@
           >
             {#each educations as education}
               <option
-                selected={education_id === education.UUID}
+                selected={importData.education_id === education.UUID}
                 value={education.UUID}>{education.name}</option
               >
             {/each}
