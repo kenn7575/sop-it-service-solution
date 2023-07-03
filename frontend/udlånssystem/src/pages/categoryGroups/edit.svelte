1d3d5ae4c77ab443547174bf7ea4cd65ab1356d2
Nima - Skole <nima0100@edu.sde.dk>
2023-07-03 15:10:03 +0200

    category_groups fix

--- frontend/udlånssystem/src/pages/categoryGroups/edit.svelte
+++ frontend/udlånssystem/src/pages/categoryGroups/edit.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import axios from "axios";
-  import validateInputs from "../../services/validateInputs.js";
-  import doesObjectsMatch from "../../services/doesObjectsMatch.js";
-  import deleteItem from "../../data/delete.js";
   import { categoryGroupModel } from "../../types/categoryGroupModel.js";
+  import deleteItem from "../../data/delete.js";
+  import update from "../../data/update.js";
   import TextQuestion from "../../components/textQuestion.svelte";
   import FormEditPanel from "../../components/form-edit-panel.svelte";
   import goToPath from "../../services/goToPath.js";
-  import updateItem from "../../data/update.js";
+  import doesObjectsMatch from "../../services/doesObjectsMatch.js";
 
   //this is the id of the brand to be edited
   export let id;
@@ -20,7 +19,9 @@
   let importData: categoryGroupModel;
   let exportData: categoryGroupModel;
 
-  //get all data
+  let table = "category_groups";
+  let page_name = "Kategorigrupper";
+
   onMount(async () => {
     try {
       importDataFromDB();
@@ -28,69 +29,72 @@
       console.log(error);
     }
   });
+
   async function importDataFromDB() {
     const { data } = await axios("get_data.php", {
-      params: { UUID: id, table: "category_groups" },
+      params: { UUID: id, table: table },
     });
     exportData = new categoryGroupModel({ ...data });
     importData = new categoryGroupModel({ ...data });
   }
 
-  function handleUpdate() {
-    if (!validateInputs()) {
-      alert("Udfyld alle felter");
-      return;
-    }
-    console.log(importData, exportData);
+  async function handleUpdate(): Promise<any> {
     if (doesObjectsMatch(importData, exportData)) {
       alert("Ingen ændringer");
       return;
     }
-
-    updateItem(importData, exportData, "category_groups").then((res) => {
-      if (res) {
-        importDataFromDB();
-        editMode = false;
-      }
-    });
+    if (!exportData.validate()) {
+      alert("Udfyld alle felter");
+      return;
+    }
+    const response: any = await update(importData, exportData, table);
+    if (response && response.success) {
+      importDataFromDB();
+      editMode = false;
+      alert("Changes saved");
+    } else {
+      alert("Error 500 - Ukendt fejl");
+    }
   }
 
   function handleDelete() {
     deleteItem(
       "delete_data.php",
-      {
-        UUID: importData.UUID,
-        table: "category_groups",
-      },
-      "/kategorigrupper"
+      { UUID: importData.UUID, table: table },
+      `/${page_name.toLowerCase()}`
     );
   }
+  
   function handleSubmit(event) {
     event.preventDefault();
     handleUpdate();
   }
 </script>
 
-{#if importData}
-  <div class="content">
-    <FormEditPanel
-      on:cancel={() => {
-        goToPath("/kategorigrupper");
-      }}
-      on:reset={importDataFromDB}
-      on:delete={handleDelete}
-      on:update={handleUpdate}
-      bind:editMode
-    />
+<svelte:head>
+  <title>{document.title} | {page_name} ~ {id}</title>
+</svelte:head>
 
-    <div class="form">
-      <form on:submit={handleSubmit} id="user-form">
-        <TextQuestion
-          bind:binding={exportData.name}
-          label="Navn"
-          required
-          {editMode}
-        />
+{#if importData}
+<div class="content">
+  <FormEditPanel
+    on:cancel={() => {
+      goToPath(`/${page_name.toLowerCase()}`);
+    }}
+    on:reset={importDataFromDB}
+    on:delete={handleDelete}
+    on:update={handleUpdate}
+    bind:editMode
+  />
+    <div
+      on:submit={(e) => {
+        e.preventDefault;
+        handleUpdate();
+      }}
+      class="form"
+    >
+      <form id="user-form">
+        <TextQuestion bind:binding={exportData.name} label="Navn" {editMode} />
       </form>
     </div>
   </div>
@@ -105,3 +109,4 @@
     gap: 1rem;
   }
 </style>
+
