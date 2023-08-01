1617a73ce825a8efe149bb698d0256da435c94ff
Kenni Kollemorten <kenn7575@gmail.com>
2023-08-01 10:32:44 +0200

    removed unused folder

--- backend/Database/delete_event.sql
+++
@@ -1,9 +0,0 @@
-DELIMITER $$
-CREATE EVENT delete_event
-    ON SCHEDULE
-        EVERY 1 HOUR
-        STARTS CURRENT_TIMESTAMP
-    DO
-        DELETE FROM login_session WHERE expiration_date < NOW() - INTERVAL 1 DAY;
-$$
-DELIMITER ;
--- backend/Database/select_user.sql
+++
@@ -1,9 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `select_user`(user_id INT, user_password VARCHAR(255))
-BEGIN
-	-- u.UUID, u.username, u.name, e.UUID, e.name
-	SELECT * FROM users u
-    LEFT JOIN educations e ON u.education_id = e.UUID
-    LEFT JOIN addresses a ON u.address_id = a.UUID
-    LEFT JOIN roles r on u.role_id = r.UUID
-     WHERE u.UUID = user_id AND u.username = user_password;
-END
