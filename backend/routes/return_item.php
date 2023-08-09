3540066219a7aca46ce9babcbc546d91e2fe4df7
Nima - Skole <nima0100@edu.sde.dk>
2023-08-09 14:04:13 +0200

    return loan system done

--- backend/routes/return_item.php
+++ backend/routes/return_item.php
@@ -2,10 +2,14 @@
 include "components/admin_db_conn.php";
 include "components/functions.php";
 
-$ids = (Array) $_GET["ids"] ?? null;
-
-if (!isset($UUID)) die(json_encode(['message'=>'UUID not set', 'status'=>400], JSON_PRETTY_PRINT));
+$ids = json_decode($_GET["ids"] ?? die(json_encode(['message'=>'ids not set', 'status'=>400], JSON_PRETTY_PRINT)));
 
 $conn->query("START TRANSACTION;");
+
+foreach ($ids as $id) {
+    $conn->query("UPDATE `items` SET `product_status_id` = 1 WHERE `UUID` = '$id'");
+}
+
+$conn->query("COMMIT;");
 
 die(json_encode($ids, JSON_PRETTY_PRINT));
--- backend/routes/return_loan.php
+++ backend/routes/return_loan.php
@@ -2,26 +2,17 @@
 include "components/admin_db_conn.php";
 include "components/functions.php";
 
-$id = $_GET["id"] ?? null;
-
-if (!isset($id)) die(json_encode(['message'=>'id not set', 'status'=>400], JSON_PRETTY_PRINT));
+$id = $_GET["id"] ?? die(json_encode(['message'=>'id not set', 'status'=>400], JSON_PRETTY_PRINT));
 
 $conn->query("START TRANSACTION;");
 
-$loan = $conn->query("SELECT * FROM `loans` WHERE `UUID` = '$id'")->fetch_object();
+$items = $conn->query("SELECT i.*
+FROM items_in_loan il
+JOIN items i ON il.item_id = i.UUID
+WHERE il.loan_id = '$id' AND i.product_status_id = 4");
 
-$items_in_loan_array = $conn->query("SELECT * FROM `items_in_loan` WHERE `loan_id` = '$loan->UUID' AND `date_returned` IS NULL")->fetch_all(MYSQLI_ASSOC);
-
-$items_in_loan = [];
-foreach ($items_in_loan_array as $item_in_loan) {
-    $items_in_loan[] = $item_in_loan;
-    $conn->query("UPDATE `items_in_loan` SET `date_returned` = NOW() WHERE `UUID` = '$item_in_loan[UUID]'");
-}
-
-
-foreach ($items_in_loan as $item_in_loan) {
-    $conn->query("UPDATE `items` SET `product_status_id` = 1 WHERE `UUID` = '$item_in_loan[item_id]'");
-    $items[] = $conn->query("SELECT * FROM `items` WHERE `UUID` = '$item_in_loan[item_id]'")->fetch_all(MYSQLI_ASSOC);
+foreach ($items as $item) {
+    $conn->query("UPDATE `items` SET `product_status_id` = 1 WHERE `UUID` = '$item[UUID]'");
 }
 
 $conn->query("COMMIT;");
@@ -29,7 +20,7 @@ $conn->query("COMMIT;");
 $result = (object) [
     "success" => true,
     "id" => $id,
-    "data" => $loan
+    "data" => $items
 ];
 
 die(json_encode($result, JSON_PRETTY_PRINT));
