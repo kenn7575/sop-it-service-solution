348c0824f537d01ac281889144d8e5460de31a65
Nima - Skole <nima0100@edu.sde.dk>
2023-08-23 12:46:23 +0200

    deleted prisma

--- backend/prisma/views/sop/products_view.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  `sop`.`products`.`UUID` AS `UUID`,
-  `sop`.`products`.`name` AS `Navn`,
-  `sop`.`products`.`date_created` AS `Dato oprettet`,
-  `sop`.`products`.`date_updated` AS `Dato opdateret`,
-  `sop`.`products`.`image_name` AS `image name`
-FROM
-  `sop`.`products`
