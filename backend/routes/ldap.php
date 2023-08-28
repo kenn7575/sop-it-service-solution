5006b2f56d5f787c1fd6525af055d1cac9bcd4c2
Nima - Skole <nima0100@edu.sde.dk>
2023-08-28 15:17:24 +0200

    added JSON_response function

--- backend/routes/ldap.php
+++ backend/routes/ldap.php
@@ -1,46 +1,31 @@
 <?php
 include 'components/headers.php';
+include 'components/functions.php';
 
 $ldap = ldap_connect($env['LDAP_HOST'], $env['LDAP_PORT']);
 
-if (!$ldap) {
-    echo json_encode([
-        'status' => 'error',
-        'message' => 'LDAP connection failed'
-    ]);
-    exit;
+if (!$ldap) { res(500, 'LDAP connection failed');
 } else {
     ldap_bind($ldap, $env['LDAP_USERNAME'], $env['LDAP_PASSWORD']);
     
     $username = $_GET['username'] ?? "";
     $password = $_GET['password'] ?? "";
 
-    if (empty($username) || empty($password)) {
-        echo json_encode([
-            'status' => 'error',
-            'message' => 'Username or password is empty'
-        ]);
-        exit;
-    }
+    if (empty($username) || empty($password)) res(400, 'Username or password is empty');
 
     $filter = "(sAMAccountName=$username)";
     $attr = array("cn");
     $result = @ldap_search($ldap, $env['LDAP_USERS'], $filter, $attr)
-    or die ("Error in search query: ".ldap_error($ldap));
+    or die(res(500, 'LDAP search failed'));
 
     $user = ldap_get_entries($ldap, $result);
 
-    if (empty($user) || empty($user[0]) || empty($user[0]['cn'])) {
-        echo json_encode([
-            'status' => 'error',
-            'message' => 'User not found'
-        ]);
-        exit;
-    }
+    if (empty($user) || empty($user[0]) || empty($user[0]['cn'])) res(400, 'User not found');
 
     $user_cn = $user[0]['cn'][0];
 
-    echo @ldap_bind($ldap, $user_cn, $password) or die("0");
+    if (@ldap_bind($ldap, $user_cn, $password)) res(200, 'Login successful');
+    else res(400, 'Invalid username or password');
 
     ldap_unbind($ldap);
 }
