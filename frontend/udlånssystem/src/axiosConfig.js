ab88d06d152ac3ac0a01c2cbfb732d3c311da38e
Nima - Skole <nima0100@edu.sde.dk>
2024-01-17 15:31:42 +0100

    Frontend refactoring for express backend (not done)

--- frontend/udlånssystem/src/axiosConfig.js
+++ frontend/udlånssystem/src/axiosConfig.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-axios.defaults.headers.common["Authorization"] = import.meta.env.VITE_APP_AUTH;
 axios.defaults.headers.post[
   "Content-Type"
 ] = `application/x-www-form-urlencoded`;
-axios.defaults.baseURL = import.meta.env.VITE_APP_BACKEND_URL;
-axios.defaults.params = {
-  user: import.meta.env.VITE_APP_ADMIN_USERNAME,
-  password: import.meta.env.VITE_APP_ADMIN_PASSWORD,
-};
+axios.defaults.baseURL = import.meta.env.VITE_APP_BACKEND_URL + "/api";
+axios.defaults.withCredentials = true;
