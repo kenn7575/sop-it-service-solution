ac06d9245476a9aa75a20de0fd714565780968b9
Nima Badiei <nimabadiei@gmail.com>
2024-01-17 18:57:09 +0100

    backend related fixes

--- frontend/udlånssystem/src/axiosConfig.js
+++ frontend/udlånssystem/src/axiosConfig.js
@@ -3,5 +3,5 @@ import axios from "axios";
 axios.defaults.headers.post[
   "Content-Type"
 ] = `application/x-www-form-urlencoded`;
-axios.defaults.baseURL = import.meta.env.VITE_APP_BACKEND_URL + "/api";
+axios.defaults.baseURL = import.meta.env.VITE_APP_BACKEND_URL;
 axios.defaults.withCredentials = true;
