a26813f6484957b66f3e1cd38bd7e60dece76351
Nima - Skole <nima0100@edu.sde.dk>
2024-01-18 15:33:53 +0100

    Added functionality to add, edit and delete regular table content

--- backend_express/routes/tables.js
+++ backend_express/routes/tables.js
@@ -2,16 +2,26 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../db");
 
+router.use((req, res, next) => {
+  if (req.path !== "/") return next();
+
+  if (!req.query?.table)
+    return res.status(400).json({ error: "Invalid table name" });
+  next();
+});
+
 router.get("/", async (req, res) => {
   const table = req.query.table;
   const UUID = (req.query.UUID && `WHERE UUID = '${req.query.UUID}'`) || "";
 
-  if (!table) return res.status(400).json({ error: "Invalid table name" });
+  const conn = await pool.getConnection();
+
+  await conn.beginTransaction();
 
   var rows;
 
   try {
-    [rows] = await pool.query(`SELECT * FROM ${table} ${UUID}`);
+    [rows] = await conn.query(`SELECT * FROM ${table} ${UUID}`);
   } catch (err) {
     if (err.code == "ER_NO_SUCH_TABLE") {
       console.log("Invalid table name:", table);
@@ -19,12 +29,98 @@ router.get("/", async (req, res) => {
     }
   }
 
+  await conn.commit();
+
+  conn.release();
+
   var result = rows;
 
   if (rows.length === 1 && UUID) result = rows[0];
   if (rows.length < 1) result = [{ error: "No results found" }];
 
   return res.json(result);
+});
+
+router.post("/", async (req, res) => {
+  const table = req.query.table;
+  const values = req.body.data;
+
+  var rows;
+
+  const conn = await pool.getConnection();
+
+  await conn.beginTransaction();
+
+  try {
+    [rows] = await pool.query(`INSERT INTO ${table} SET ?`, values);
+  } catch (err) {
+    if (err.code == "ER_NO_SUCH_TABLE") {
+      console.log("Invalid table name:", table);
+      return res.status(400).json({ error: "Invalid table name" });
+    }
+    console.log(err);
+  }
+
+  await conn.commit();
+
+  conn.release();
+
+  return res.json({ id: rows.insertId });
+});
+
+router.patch("/", async (req, res) => {
+  const table = req.query.table;
+  const UUID = req.query.UUID;
+  const values = req.body.data;
+
+  var rows;
+
+  const conn = await pool.getConnection();
+
+  await conn.beginTransaction();
+
+  try {
+    [rows] = await pool.query(`UPDATE ${table} SET ? WHERE UUID = '${UUID}'`, [
+      values,
+    ]);
+  } catch (err) {
+    if (err.code == "ER_NO_SUCH_TABLE") {
+      console.log("Invalid table name:", table);
+      return res.status(400).json({ error: "Invalid table name" });
+    }
+  }
+
+  await conn.commit();
+
+  conn.release();
+
+  return res.json({ id: UUID });
+});
+
+router.delete("/", async (req, res) => {
+  const table = req.query.table;
+  const UUID = req.query.UUID;
+
+  var rows;
+
+  const conn = await pool.getConnection();
+
+  await conn.beginTransaction();
+
+  try {
+    [rows] = await pool.query(`DELETE FROM ${table} WHERE UUID = '${UUID}'`);
+  } catch (err) {
+    if (err.code == "ER_NO_SUCH_TABLE") {
+      console.log("Invalid table name:", table);
+      return res.status(400).json({ error: "Invalid table name" });
+    }
+  }
+
+  await conn.commit();
+
+  conn.release();
+
+  return res.json(rows);
 });
 
 module.exports = router;
