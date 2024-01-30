fed2e5a09dc2c5b303caf1624e034ba63f0607b1
Nima - Skole <nima0100@edu.sde.dk>
2024-01-30 11:53:11 +0100

    Fix: resolves not being able to log in (#28)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,57 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { authenticate } = require("ldap-authentication");
+const { ldapAuthenticate } = require("../functions/auth");
 const jwt = require("jsonwebtoken");
-
-const userSearchBase =
-  process.env.NODE_ENV == "development"
-    ? process.env.LDAP_USERS
-    : process.env.LDAP_ADMINS;
-
-let options = {
-  ldapOpts: {
-    url: `ldap://${process.env.LDAP_HOST}:${process.env.LDAP_PORT}`,
-  },
-  adminDn: process.env.LDAP_USERNAME,
-  adminPassword: process.env.LDAP_PASSWORD,
-  userSearchBase,
-  usernameAttribute: "samaccountname",
-  attributes: ["sn", "givenName", "Name", "sAMAccountName", "mail"],
-};
 
 router.post("/login", async (req, res) => {
   const { username, password: userPassword } = req.body;
-
-  if (process.env.NODE_ENV == "development") {
-    const user = {
-      UUID: 792,
-      username: "dev",
-      name: "Dev",
-      mail: "dev@dev.com",
-    };
-
-    const token = jwt.sign(user, process.env.JWT_SECRET);
-
-    res.cookie("token", token, {
-      httpOnly: true,
-      sameSite: "none",
-      secure: true,
-    });
-
-    return res.json(user);
-  }
 
   if (!username || !userPassword)
     return res.json({ error: "Missing credentials" });
 
   try {
-    let ADUser = await authenticate({ ...options, username, userPassword });
-
-    const user = {
-      username: ADUser.sAMAccountName,
-      name: ADUser.name,
-      mail: ADUser.mail,
-    };
+    let user = await ldapAuthenticate(username, userPassword);
 
     const token = jwt.sign(user, process.env.JWT_SECRET, {
       expiresIn: "10m",
@@ -65,20 +24,8 @@ router.post("/login", async (req, res) => {
 
     res.json(user);
   } catch (err) {
-    console.log(err)
-    if (err?.admin?.lde_message) {
-      console.log("Invalid admin credentials");
-      res.json({ error: "Invalid admin credentials" });
-    } else if (err?.lde_message) {
-      console.log("Invalid credentials");
-      res.json({ error: "Invalid credentials" });
-    } else if (err?.name == "LdapAuthenticationError") {
-      console.log("User not found");
-      res.json({ error: "User not found" });
-    } else {
-      console.log(err);
-      res.json({ error: "Something went wrong" });
-    }
+    console.log(err);
+    res.json({ error: "Something went wrong" });
   }
 });
 
@@ -102,7 +49,7 @@ router.post("/validate", async (req, res) => {
 
 router.post("/validate_password", async (req, res) => {
   console.log(req.body);
-})
+});
 
 router.get("/cookies", (req, res) => {
   res.json(req.cookies);
