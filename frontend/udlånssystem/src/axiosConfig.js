0f7038cca73778ee14f14caff22210d5e27cfd9b
kenni kollemorten <kenn7575@gmail.com>
2024-01-30 12:05:12 +0100

    init

--- frontend/udlånssystem/src/axiosConfig.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-axios.defaults.headers.post[
-  "Content-Type"
-] = `application/x-www-form-urlencoded`;
-axios.defaults.baseURL = import.meta.env.VITE_APP_BACKEND_URL;
-axios.defaults.withCredentials = true;
--- frontend/udlånssystem/src/services/doesObjectsMatch.js
+++
@@ -1,4 +0,0 @@
-export default function doesObjectsMatch(obj1, obj2) {
-  if (JSON.stringify(obj1) !== JSON.stringify(obj2)) return false;
-  return true;
-}
