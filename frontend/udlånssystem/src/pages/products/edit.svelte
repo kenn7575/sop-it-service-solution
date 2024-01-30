0954dfd7a8567430696009c9049e55cf55e453b6
Nima Badiei <nimabadiei@gmail.com>
2024-01-30 20:41:46 +0100

    Fixed: See which loan an item is lent out to #30

--- frontend/udlånssystem/src/pages/products/edit.svelte
+++ frontend/udlånssystem/src/pages/products/edit.svelte
@@ -30,8 +30,11 @@
   let product_status: productStatusModel[] = [];
   let products: productModel[] = [];
 
+  let itemLoan: { UUID: number, loan_id: number } = null;
+
   async function importDataFromDB() {
     let data = await getData(table, id);
+    itemLoan = await getData("items_from_loans", id);
 
     // HOT FIX - if the data is not found, redirect to the index page
     if (!data?.UUID) {
@@ -97,7 +100,7 @@
     handleUpdate();
   }
   import { path } from "../../stores/pathStore.js";
-  $: console.log($path);
+  // $: console.log($path);
 </script>
 
 {#if importData}
@@ -110,6 +113,8 @@
       on:delete={handleDelete}
       on:update={handleUpdate}
       bind:editMode
+      loanId={itemLoan?.loan_id}
+      item={exportData}
     />
     <div
       on:submit={(e) => {
