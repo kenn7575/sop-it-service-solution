d85bed857236756de558ba3650ee767911a41f20
Nima Badiei <nimabadiei@gmail.com>
2024-01-30 19:53:52 +0100

    Changed developer login to not require actual login + changed default backend port from 3000 -> 5000

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use("/api/loans", require("./routes/loans"));
 app.use("/api/mail", require("./routes/mail"));
 app.use("/api", require("./routes/tables"));
 
-const port = process.env.BACKEND_PORT || 3000;
+const port = process.env.BACKEND_PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
