8b8213eeea27c81378263aa4707695302c0b3840
Nima Badiei <nimabadiei@gmail.com>
2024-06-21 14:16:24 +0200

    Removed fields from user in database

--- backend/prisma/views/sop/users_view.sql
+++ backend/prisma/views/sop/users_view.sql
@@ -2,7 +2,6 @@ SELECT
   `u`.`UUID` AS `UUID`,
   `u`.`username` AS `Brugernavn`,
   `u`.`date_created` AS `Oprettet`,
-  `u`.`date_updated` AS `Opdateret`,
-  `u`.`mail` AS `Mail`
+  `u`.`date_updated` AS `Opdateret`
 FROM
   `sop`.`users` `u`
