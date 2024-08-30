ecbae6d07d4dca4426f1126feba5164be7ab2570
Nima Badiei <nimabadiei@gmail.com>
2024-08-30 08:48:29 +0200

    refactor: Remove unused label import in Feedback page

--- frontend/udlånssystem/src/pages/Feedback.tsx
+++ frontend/udlånssystem/src/pages/Feedback.tsx
@@ -1,5 +1,4 @@
 import { Input } from '@components/ui/input';
-import { Label } from '@components/ui/label';
 import { Textarea } from '@components/ui/textarea';
 
 import createItem from '@data/create';
