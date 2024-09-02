5a3f509bd939bf3e350fe316d24b3d5727b4c21a
Nima Badiei <nimabadiei@gmail.com>
2024-09-02 13:14:19 +0200

    Formatted with prettier

--- frontend/udlånssystem/src/components/ui/table.tsx
+++ frontend/udlånssystem/src/components/ui/table.tsx
@@ -51,7 +51,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      'hover:bg-muted/50 data-[state=selected]:bg-muted transition-colors',
+      'hover:bg-muted/50 transition-colors data-[state=selected]:bg-muted',
       className,
     )}
     {...props}
