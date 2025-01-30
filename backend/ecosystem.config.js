7f4a246bd613947f5e545660afb3b64b353a667a
Nima Badiei <nimabadiei@gmail.com>
2025-01-30 10:54:23 +0100

    Formatted file

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -4,8 +4,9 @@ module.exports = {
       name: "SOP",
       script: "dist/src/index.js",
       args: "--port 5000",
-      node_args: "-r ts-node/register/transpile-only -r tsconfig-paths/register -r dotenv/config",
-      watch: true
-    }
-  ]
-}
+      node_args:
+        "-r ts-node/register/transpile-only -r tsconfig-paths/register -r dotenv/config",
+      watch: true,
+    },
+  ],
+};
